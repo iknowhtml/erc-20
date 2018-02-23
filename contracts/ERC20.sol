ed63181feb28a1d44f66271a50956114ae06e8f8
Aki Gao <xag9bb@virginia.edu>
2018-02-23 10:01:02 -0500

    Remove solhint warning disable line

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -13,7 +13,6 @@ contract ERC20 {
     function approve(address _spender, uint256 _value) public returns (bool success);
     function allowance(address _owner, address _spender) public view returns (uint256 remaining);
 
-    // solhint-disable-next-line no-simple-event-func-name
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
