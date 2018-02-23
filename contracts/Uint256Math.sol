2be423a56663745acc4d83042d2e84629f2e243a
Aki Gao <xag9bb@virginia.edu>
2018-02-22 21:06:52 -0500

    Add library to prevent addition and subtraction overflow

---
+++ contracts/Uint256Math.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.19;
+
+/**
+ * @dev Math operations with checks to prevent overflows
+ */
+
+library Uint256Math {
+    /**
+    * @dev Substracts two numbers, throws if value overflows.
+    * param a Value to be subtracted.
+    * param b Value to subtract.
+    * return Result of subtracting b from a
+    */
+    function subtract(uint256 a, uint256 b) internal pure returns (uint256) {
+        // check to insure that the result is not negative (overflow)
+        assert(b <= a);
+        return a - b;
+    };
+
+    /**
+    * param a Value to be add to.
+    * param b Value to add.
+    * return Result of adding a and b.
+    * @dev Adds two numbers, throws if value overflows.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        // check to insure that the result did not overflow.
+        assert(c >= a);
+        return c;
+    }
+}
