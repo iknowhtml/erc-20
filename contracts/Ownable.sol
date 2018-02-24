91fa6a314f4ca0854296d9071511e781540f2ebc
Aki Gao <xag9bb@virginia.edu>
2018-02-23 21:10:37 -0500

    Add Ownable smart contract

---
+++ contracts/Ownable.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.19;
+
+
+contract Ownable {
+    address public owner;
+
+    /**
+    * @dev Constructor for Ownable contract. Sets contract owner to address that deployed the contract.
+    **/
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+    /*
+    * @dev Modifier to ensure that function is being executed by only the owner of the contract.
+    */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        // check to insure that the new owner's address is not 0x0.
+        // otherwise, the contract will permanently be unusable.
+        require(newOwner != 0x0);
+
+        owner = _newOwner;
+    }
+}
