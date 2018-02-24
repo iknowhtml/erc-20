3260756c932f92f4cd38f519ac0ad90d023c9c25
Aki Gao <xag9bb@virginia.edu>
2018-02-23 22:09:34 -0500

    Format code, edit documentation

--- contracts/Uint256Math.sol
+++ contracts/Uint256Math.sol
@@ -13,8 +13,10 @@ library Uint256Math {
     **/
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
+
         // check to insure that the result did not overflow.
         assert(c >= a);
+
         return c;
     }
 
@@ -22,12 +24,28 @@ library Uint256Math {
     * @dev Substracts two numbers, throws if value overflows.
     * param a Value to be subtracted.
     * param b Value to subtract.
-    * return Result of subtracting b from a
+    * return Result of subtracting b from a.
     **/
     function subtract(uint256 a, uint256 b) internal pure returns (uint256) {
         // check to insure that the result is not negative (overflow)
         assert(b <= a);
+
         return a - b;
+    }
+
+    /**
+    * @dev Multiples two numbers, throws if value overflows.
+    * param a Value to be multiplied.
+    * param b Value to multiply.
+    * return Result of multiplying a by b.
+    **/
+    function multiply(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint c = a / b;
+
+        //check to insure that the result did not overflow.
+        assert(c / a == b);
+
+        return c;
     }
 
 
