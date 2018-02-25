edbe4442b9789c50c98917585e5314305d62b34e
Aki Gao <xag9bb@virginia.edu>
2018-02-25 10:45:41 -0500

    Add tests for Ownable contract

---
+++ test/Ownable.test.js
@@ -0,0 +1,73 @@
+import { assert, expect, should } from 'chai';
+import web3 from './setupWeb3';
+
+import compiledOwnable from '../build/contracts/Ownable.json';
+
+import contractFactory from './contractFactory';
+
+let accounts, ownable, contractOwner;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  contractOwner = accounts[0];
+  ownable = await contractFactory(compiledOwnable, contractOwner);
+});
+
+describe('Ownable', () => {
+  it('Should initialize an Ownable contract to be owned by the correct owner', async () => {
+    const owner = await ownable.methods.owner().call();
+    expect(owner, 'Owner is not correct').to.equal(contractOwner);
+  });
+
+  it('Should transfer ownership to a different owner and emit an OwnershipTransfered event', async () => {
+    const newContractOwner = accounts[1];
+    const {
+      events: { OwnershipTransfered: { event } },
+    } = await ownable.methods
+      .transferOwnership(newContractOwner)
+      .send({ from: contractOwner, gas: '1000000' });
+
+    expect(event, 'Event was not correct').to.equal('OwnershipTransfered');
+
+    const owner = await ownable.methods.owner().call();
+
+    expect(owner, 'Owner is not correct').to.equal(newContractOwner);
+  });
+
+  it('Should fail transferring ownership to 0x0', async () => {
+    const { events } = await ownable.methods
+      .transferOwnership('0x0')
+      .send({ from: contractOwner, gas: '1000000' });
+
+    expect(events, 'Event was not empty').to.be.empty;
+
+    const owner = await ownable.methods.owner().call();
+
+    expect(owner, 'Owner is not correct').to.equal(contractOwner);
+  });
+
+  it('Should fail transferring ownership to current contract owner', async () => {
+    const { events } = await ownable.methods
+      .transferOwnership(contractOwner)
+      .send({ from: contractOwner, gas: '1000000' });
+
+    expect(events, 'Event was not empty').to.be.empty;
+
+    const owner = await ownable.methods.owner().call();
+
+    expect(owner, 'Owner is not correct').to.equal(contractOwner);
+  });
+
+  it('Should fail transferring ownership from an account that is not the current contract owner', async () => {
+    const differentOwner = accounts[1];
+    const { events } = await ownable.methods
+      .transferOwnership(differentOwner)
+      .send({ from: differentOwner, gas: '1000000' });
+
+    expect(events, 'Event was not empty').to.be.empty;
+
+    const owner = await ownable.methods.owner().call();
+
+    expect(owner, 'Owner is not correct').to.equal(contractOwner);
+  });
+});
