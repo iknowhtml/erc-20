433d06242bda013630ece1bd07fd92dfb9a2e255
Aki Gao <xag9bb@virginia.edu>
2018-02-25 10:47:19 -0500

    Add account parameter to contractFactory library function

--- test/contractFactory.js
+++ test/contractFactory.js
@@ -1,11 +1,11 @@
 import web3 from './setupWeb3';
 
-async function contractFactory(compiledContract) {
+async function contractFactory(compiledContract, account) {
   const accounts = await web3.eth.getAccounts();
 
   const contractInstance = await new web3.eth.Contract(compiledContract.abi)
     .deploy({ data: compiledContract.bytecode })
-    .send({ from: accounts[0], gas: '4700000' });
+    .send({ from: account, gas: '4700000' });
 
   contractInstance.setProvider(web3.currentProvider);
 
