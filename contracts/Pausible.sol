0a46e28b5cd75b2b7041767974e4dd179bbe570d
Aki Gao <xag9bb@virginia.edu>
2018-02-25 10:46:49 -0500

    Add Pause and Unpause event

--- contracts/Pausible.sol
+++ contracts/Pausible.sol
@@ -7,6 +7,9 @@ contract Pausible is Ownable {
     //defaults to false
     bool public paused;
 
+    event Paused();
+    event Unpaused();
+
     /**
     * @dev Modifier to ensure a function is called when only when the contract isn't paused.
     **/
@@ -16,16 +19,26 @@ contract Pausible is Ownable {
     }
 
     /**
-    * @dev Function to pause the contract. Can only be called by the contract owner.
+    * @dev Modifier to ensure a function is called when only when the contract is paused.
     **/
-    function pause() public onlyOwner {
-        paused = true;
+    modifier whenPaused() {
+        require(paused == true);
+        _;
     }
 
     /**
-    * @dev Function to unpause the contract. Can only be called by the owner.
+    * @dev Function to pause the contract. Can only be called by the contract owner when the contract is not paused.
     **/
-    function unpaused() public onlyOwner {
+    function pause() public onlyOwner whenNotPaused {
+        paused = true;
+        Paused();
+    }
+
+    /**
+    * @dev Function to unpause the contract. Can only be called by the owner when the contract is paused.
+    **/
+    function unpause() public onlyOwner whenPaused {
         paused = false;
+        Unpaused();
     }
 }
