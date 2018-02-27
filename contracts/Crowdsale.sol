48e698179596aec214f2b64385ff5452bf60adad
Aki Gao <xag9bb@virginia.edu>
2018-02-26 21:41:21 -0500

    Add skeleton for Crowdsale contract

---
+++ contracts/Crowdsale.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.19;
+
+import "./Uint256Math.sol";
+import "./SampleToken.sol";
+
+contract Crowdsale is Ownable{
+    using Uint256Math for uint256;
+    SampleToken public sampleToken;
+    uint256 public rate;
+    uint256 public weiRaised;
+
+    mapping(address => uint256) public allocations;
+
+    modifier crowdSaleIsGoing() {
+        require(block.timestamp >= startTime && block.timestamp < block.timestamp);
+        _;
+    }
+
+    modifier crowdsaleHasEnded() {
+        require(block.timestamp >= endTime);
+        _;
+    }
+
+    function() external payable {
+        purchaseTokens(msg.sender);
+    }
+
+    function purchaseTokens(uint256 _address) public;
+    function claimTokens() public;
+
+    function _preValidatePurchase(address _buyer) internal;
+    function _postValidatePurchase(address _buyer) internal;
+
+
+
+}
