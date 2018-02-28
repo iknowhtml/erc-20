15b2aba36d1893059a389a45ed41534ba838a8fa
Aki Gao <xag9bb@virginia.edu>
2018-02-27 21:45:02 -0500

    Refactor Pausable functionality to Pausible Token smart contract

--- contracts/SampleToken.sol
+++ contracts/SampleToken.sol
@@ -1,52 +1,14 @@
 pragma solidity ^0.4.19;
 
 import "./Token.sol";
-import "./Pausible.sol";
+import "./PausableToken.sol";
 
 
-contract SampleToken is Token, Pausible {
-
-    // Parameters for determining characteristics of the token
-    string private constant NAME = "Sample Token";
-    string private constant SYMBOL = "SAMT";
-    uint8 private constant DECIMALS = 18;
-    uint private constant TOKEN_SUPPLY = 100;
-
+contract SampleToken is Token, PausableToken {
     /**
     * @dev Constructor function for Sample Token.
     **/
-    function SampleToken() public Token(NAME, SYMBOL, DECIMALS, TOKEN_SUPPLY) {
-    }
-
-    /**
-    * @dev Transfers a certain amount of tokens to an address when the contract is not paused.
-    * @param _to The address of the recipient.
-    * @param _value The amount of token to be transferred.
-    * @return Whether the transfer was successful or not.
-    **/
-    function transfer(address _to, uint256 _value) public whenNotPaused returns (bool success) {
-        return super.transfer(_to, _value);
-    }
-
-    /**
-    * @dev Transfers a certain amount of tokens from a given address to another address when the contract is not paused.
-    * (as long as the transfer has been approved by the from address)
-    * @param _from The address of the sender.
-    * @param _to The address of the recipient.
-    * @param _value The amount of token to be transferred.
-    * @return Whether the transfer was successful or not.
-    **/
-    function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool success) {
-        return super.transferFrom(_from, _to, _value);
-    }
-
-    /**
-    * @dev Approves (but does not send) the transfer of tokens from an address when contract is not paused.
-    * @param _spender The address of the account able to transfer the tokens.
-    * @param _value The amount of tokens to be approved for transfer.
-    * @return Whether the approval was successful or not.
-    **/
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        return super.approve(_spender, _value);
+    function SampleToken(string _name, string _symbol, uint8 _decimals, uint256 _supply) public
+    Token(_name, _symbol, _decimals, _supply) {
     }
 }
