97615f88be868f17297be0e47b8f5e33cc053260
Aki Gao <xag9bb@virginia.edu>
2018-02-27 21:45:26 -0500

    Add functionality to Crowdsale contract

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,29 +1,46 @@
 pragma solidity ^0.4.19;
 
 import "./Uint256Math.sol";
-import "./SampleToken.sol";
+import "./Ownable.sol";
+import "./ERC20.sol";
 
 
 contract Crowdsale is Ownable {
     using Uint256Math for uint256;
 
-    SampleToken public sampleToken;
+    ERC20 public token;
+    address public tokenAddress;
     uint256 public rate;
     uint256 public weiRaised;
 
+    event FundsWithdrawn(uint256 amount);
+
     function() external payable {
-        purchaseTokens();
+        _purchaseTokens();
+    }
+
+    function Crowdsale(address _tokenAddress, uint256 _rate) public {
+        require(_tokenAddress != 0x0);
+        require(_rate != 0);
+
+        tokenAddress = _tokenAddress;
+        rate = _rate;
     }
 
     function claimTokens() external {
 
     }
 
-    function withdrawlFunds() external {
-        owner.transfer(msg.value);
+    function withdrawFunds(uint256 amount) public onlyOwner {
+        require(amount <= weiRaised);
+        weiRaised = weiRaised.subtract(amount);
+
+        owner.transfer(amount);
+
+        FundsWithdrawn(amount);
     }
 
-    function purchaseTokens() internal {
+    function _purchaseTokens() internal {
 
     }
 
