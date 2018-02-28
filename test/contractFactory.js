74579233bd7219f28fdd3f8726f8ac5e0d6943ff
Aki Gao <xag9bb@virginia.edu>
2018-02-27 21:45:46 -0500

    Update contractFactory library function to accept constructor arguments

--- test/contractFactory.js
+++ test/contractFactory.js
@@ -1,10 +1,15 @@
 import web3 from './setupWeb3';
 
-async function contractFactory(compiledContract, account) {
-  const accounts = await web3.eth.getAccounts();
-
+async function contractFactory(
+  compiledContract,
+  account,
+  ...contractArguments
+) {
   const contractInstance = await new web3.eth.Contract(compiledContract.abi)
-    .deploy({ data: compiledContract.bytecode })
+    .deploy({
+      data: compiledContract.bytecode,
+      arguments: contractArguments,
+    })
     .send({ from: account, gas: '4700000' });
 
   contractInstance.setProvider(web3.currentProvider);
